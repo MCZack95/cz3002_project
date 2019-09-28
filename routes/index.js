5b37ee562447b67c009e94db855564c7fd0ecfa9
Zhi Hong <yeo_zh@hotmail.com>
2019-09-28 15:06:35 +0800

    Added Bootstrap

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var login = require('./main_page');
+var main_page = require('./main_page');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'NTU Learning Platform', username: ''});
 });
 
+/* Use this route to make test /main easier */
+router.get('/main', function(req, res, next) {
+  console.log('Logging in via GET');
+  res.render('main_page', { title: 'Main Page', matric_id: 'Developer' });
+})
+
 router.post('/main', function(req, res, next) {
-  console.log('Post!!');
+  console.log('Logging in via POST');
   res.render('main_page', { title: 'Main Page', matric_id: req.body.matric_id });
 });
 
