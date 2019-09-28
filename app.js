81ab2491ba59b1947b316d476a34da623e5b2ab3
Zhi Hong <yeo_zh@hotmail.com>
2019-09-28 13:38:14 +0800

    1) Removed Sign up button 2) Added Matric ID field (not sure if needed)

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var signupRouter = require('./routes/signup');
-var loginRouter = require('./routes/login');
+var mainPageRouter = require('./routes/main_page');
 
 var app = express();
 
@@ -23,8 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/signup', signupRouter);
-app.use('/login', loginRouter);
+app.use('/main', mainPageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var signup = require('./signup');
-var login = require('./login');
+var login = require('./main_page');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'NTU Learning Platform', username: ''});
 });
 
-router.get('/signup', function(req, res, next) {
-  res.render('signup', { title: 'Sign Up Page'});
-});
-
-router.post('/signup', function(req, res, next) {
-  console.log(req.body.username);
-  res.render('index', { title: 'NTU Learning Platform', username: req.body.username });
-});
-
-router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Main Page'});
+router.post('/main', function(req, res, next) {
+  console.log('Post!!');
+  res.render('main_page', { title: 'Main Page', matric_id: req.body.matric_id });
 });
 
 module.exports = router;
