1ccde49b3788f20dc9fe17e01d4acbaaf20a49ac
Shan Jing <sj_chua@hotmail.com>
2019-10-01 14:57:31 +0800

    first commit by sj - firebase config

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var main_page = require('./main_page');
 
+var firebase = require('firebase');
+firebase.initializeApp(require('../firebaseconfig.json'));
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'NTU Learning Platform', username: ''});
