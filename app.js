88613b89b097b5d68ab83dcb62672db34f823284
Shan Jing <sj_chua@hotmail.com>
2019-10-13 18:04:22 +0800

    datetime on mainpage fixed, invalid dates means the ms got PROBLEM

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var createQuizRouter = require('./routes/createquiz');
 
 var app = express();
 
+app.locals.moment = require('moment');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
